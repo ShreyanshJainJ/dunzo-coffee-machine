df2796a4b06f0796e30ca486720d0fe2ea2bcbc4
Shreyansh <jshreyanshjain@gmail.com>
2021-07-18 22:29:14 +0530

    Add env variable in config

--- coffee_machine/config/machine_interface.py
+++ coffee_machine/config/machine_interface.py
@@ -1,4 +1,5 @@
 # machine configuration details and application config
+import os
 from coffee_machine.config import START_CONFIG
 
 
@@ -6,5 +7,5 @@ MACHINE_CONFIG = START_CONFIG.get("machine").get("outlets")
 INVENTORY_CONFIG = START_CONFIG.get("machine").get("total_items_quantity")
 DRINK_CONFIG = START_CONFIG.get("machine").get("beverages")
 
-INGREDIENT_LOW_THRESHOLD = 0.2
-TIME_TO_PREPARE_BREVERAGE = 5
+INGREDIENT_LOW_THRESHOLD = float(os.getenv("INGREDIENT_LOW_THRESHOLD", 0.5))
+TIME_TO_PREPARE_BREVERAGE = float(os.getenv("TIME_TO_PREPARE_BREVERAGE", 0))
