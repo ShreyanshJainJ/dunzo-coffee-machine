8cd70cbfb7d6b3e4050662db3a671c170608669b
Shreyansh <jshreyanshjain@gmail.com>
2021-07-18 22:28:29 +0530

    Machine config bug fix

--- coffee_machine/__init__.py
+++ coffee_machine/__init__.py
@@ -1,3 +1,4 @@
+import copy
 from coffee_machine.config.machine_interface import (DRINK_CONFIG,
                                                      INVENTORY_CONFIG,
                                                      MACHINE_CONFIG)
@@ -10,7 +11,7 @@ from coffee_machine.services.machine import Machine
 # machine conf details
 machine_details = Machine(
     outlets=MACHINE_CONFIG.get("count_n"),
-    item_capacity=INVENTORY_CONFIG
+    item_capacity=copy.deepcopy(INVENTORY_CONFIG)
 )
 # beverage details
 drink_details = Beverages(DRINK_CONFIG)
